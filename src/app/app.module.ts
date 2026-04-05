0d70554df2ede994f7145e2c892d3c1340cea2f8
Vigneshram0607 <vigneshram0706@gmail.com>
2026-04-05 21:26:45 +0530

    Updated all components to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,36 @@ import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BudgetComponent } from './components/budget/budget.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InsightsComponent } from './components/insights/insights.component';
+import { CountUpDirective } from './components/shared/count-up.directive';
+import { HighlightPipe } from './components/shared/highlight.pipe';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SkeletonComponent } from './components/shared/skeleton/skeleton.component';
+import { SummaryCardComponent } from './components/shared/summary-card/summary-card.component';
+import { SvgIconComponent } from './components/shared/svg-icon.component';
+import { ToastComponent } from './components/shared/toast/toast.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgIconComponent,
+    SummaryCardComponent,
+    NavbarComponent,
+    ToastComponent,
+    SkeletonComponent,
+    CountUpDirective,
+    HighlightPipe,
+    DashboardComponent,
+    TransactionsComponent,
+    InsightsComponent,
+    BudgetComponent,
   ],
   imports: [
     BrowserModule,
