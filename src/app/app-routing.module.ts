79a934e6ec711b06da1b0faaf89b316704fead39
Vigneshram0607 <vigneshram0706@gmail.com>
2026-04-05 21:26:06 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BudgetComponent } from './components/budget/budget.component';
+import { InsightsComponent } from './components/insights/insights.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 const routes: Routes = [
-  
+  { path: '',             redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard',    component: DashboardComponent   },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'insights',     component: InsightsComponent    },
+  { path: 'budget',       component: BudgetComponent,
+    data: { standalone: true }                             },
+  { path: '**',           redirectTo: 'dashboard'         },
 ];
 
 @NgModule({
